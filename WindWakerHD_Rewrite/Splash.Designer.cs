0c2209e7c6c706e385078ab136f54c7c247dc6f3
Jacob Butfiloski <jacobbutfiloski@gmail.com>
2021-04-09 00:00:10 -0400

    updated logo

--- WindWakerHD_Rewrite/Splash.Designer.cs
+++ WindWakerHD_Rewrite/Splash.Designer.cs
@@ -37,7 +37,7 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(-1, 72);
+            this.pictureBox1.Location = new System.Drawing.Point(-9, 70);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(1484, 347);
             this.pictureBox1.TabIndex = 0;
